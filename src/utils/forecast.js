ceaff6f2494926166fe3a46e18a66176524cc5d2
mac <mac@macs-MacBook-Pro.local>
2019-06-03 18:42:46 +0530

    round off temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,9 +9,12 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            const tempDegrees = (body.currently.temperature - 32) * 5/9 ;
-            const lowTemp = (body.daily.data[0].temperatureLow - 32 ) * 5/9;
-            const highTemp = (body.daily.data[0].temperatureHigh -32 ) * 5/9;
+            let tempDegrees = (body.currently.temperature - 32) * 5/9;
+            tempDegrees = Math.round(tempDegrees * 100) / 100;
+            let lowTemp = (body.daily.data[0].temperatureLow - 32 ) * 5/9;
+            lowTemp = Math.round(lowTemp * 100) / 100;
+            let highTemp = (body.daily.data[0].temperatureHigh -32 ) * 5/9;
+            highTemp = Math.round(highTemp * 100) / 100;
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + tempDegrees + ' degress out. This high today is ' + highTemp + 'with low of ' + lowTemp + ' There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
